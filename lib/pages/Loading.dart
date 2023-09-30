f577dd39150e6d5656678ec8059dd8b6a50442c9
tifani3112 <102417496+tifani3112@users.noreply.github.com>
2023-09-30 22:42:35 +0700

    finished project

--- lib/pages/Loading.dart
+++ lib/pages/Loading.dart
@@ -2,6 +2,11 @@ import 'dart:async';
 import 'package:bankapp/pages/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:supercharged/supercharged.dart';
+
+import 'package:sa4_migration_kit/sa4_migration_kit.dart';
+
+import '../Animation/fade.dart';
 
 class Loadpage extends StatefulWidget {
   @override
@@ -9,11 +14,14 @@ class Loadpage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<Loadpage> {
+
+
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(const Duration(milliseconds: 500), () {
+    Timer(const Duration(seconds: 5), () {
       Navigator
           .pushReplacement(context,PageTransition(type: PageTransitionType.fade,child: const Homepage()));
     });
@@ -22,15 +30,14 @@ class _MyHomePageState extends State<Loadpage> {
   @override
   Widget build(BuildContext context) {
     final tween = MultiTween<AniProps>()
-      ..add(AniProps.opacity, 0.0.tweenTo(1.0), 500.milliseconds)
-      ..add(AniProps.translateY, (-30.0).tweenTo(0.0), 500.milliseconds,
+      ..add(AniProps.opacity, 0.0.tweenTo(1.0), 5.seconds)
+      ..add(AniProps.translateY, (-30.0).tweenTo(0.0), 5.seconds,
           Curves.easeOut);
 
     return PlayAnimation<MultiTweenValues<AniProps>>(
-      delay: Duration(milliseconds: (500 * delay).round()),
+      delay: const Duration(seconds: 1),
       duration: tween.duration,
       tween: tween,
-      child: child,
       builder: (context, child, value) => Opacity(
         opacity: value.get(AniProps.opacity),
         child: Transform.translate(
